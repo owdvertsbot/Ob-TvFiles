2a4808355a0c983bddba696688e35eb7775da515
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2023-05-06 20:22:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,11 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "1. ғɪʀsᴛ ᴊᴏɪɴ ᴛʜᴇ <a href='http://t.me/Ob_Link'><b>ᴄʜᴀɴɴᴇʟ</b></a>
+
+<b>2. ᴛᴀᴘ ᴏɴ ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ ᴀɢᴀɪɴ ᴏʀ ʀᴇʟᴏᴀᴅ ⚡️
+
+3. ᴛᴀᴘ ᴏɴ sᴛᴀʀᴛ ᴀɴᴅ ᴅᴏɴᴇ ✅</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -58,7 +62,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "sᴇɴᴅ ᴛʜɪs ᴛᴏ ʏᴏᴜʀ ᴍᴏᴍᴍʏ"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
