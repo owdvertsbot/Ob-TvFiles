dbfe8f9fc6f2eed8e3d68daaad70cb8a32dd7482
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2023-05-07 19:13:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from bot import Bot
 
 Bot().run()
+
+SCRIPTNAME="script"
+
+SCRIPT="script.sh"
+
+echo "running $SCRIPTNAME nohup" "$@"
+
+tmux new-window -d -t "nohup-${SCRIPTNAME}": "$SCRIPT" "$@" || tmux new -d -s "nohup-${SCRIPTNAME}" "$SCRIPT" "$@"
